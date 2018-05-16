1413023a137ad9263d14d868a3f892e4fde535c3
takuya <takuya.1st+nospam@gmail.com>
2018-05-16 16:54:54 +0900

    initial import

---
+++ lockscreen/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  lockscreen
+//
+//  Created by takuya on 20180516.
+//  Copyright © 2018年 takuya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+
+    NSBundle *bnd = [NSBundle bundleWithPath:@"/Applications/Utilities/Keychain Access.app/Contents/Resources/Keychain.menu"];
+
+    Class pc = [bnd principalClass];
+    id k = [[pc alloc]init];
+
+    [k performSelector:@selector(_lockScreenMenuHit) withObject:NULL];
+  }
+  return 0;
+}
